b63aa1f5cd280b1da8959870d7345f51121fdd62
Joerg Zeidler <joerg.zeidler@conplement.de>
2022-12-19 11:54:55 +0100

    qa

--- recipes-omnect/omnect-base-files/omnect-base-files/usr/bin/omnect_get_dps_tpm_enrollment.sh
+++ recipes-omnect/omnect-base-files/omnect-base-files/usr/bin/omnect_get_dps_tpm_enrollment.sh
@@ -7,6 +7,5 @@ cd /tmp
 
 tpm2_readpublic -Q -c ${ek_address} -o ek.pub 2> /dev/null
 
-# Registration Id will be calculated as checksum from the endorsement key
-printf "Gathering the registration information...\n\nRegistration Id:\n%s\n\nEndorsement Key:\n%s\n" $(sha256sum -b ek.pub | cut -d' ' -f1 | sed -e 's/[^[:alnum:]]//g') $( base64 -w0 ek.pub)
-rm ek.pub srk.ctx 2> /dev/null
+printf "Gathering the registration information...\n\nEndorsement Key:\n%s\n" $( base64 -w0 ek.pub)
+rm ek.pub 2> /dev/null
