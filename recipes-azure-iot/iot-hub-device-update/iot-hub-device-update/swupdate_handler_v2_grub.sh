be0a7a65954ae39f90addf6152b79ed611cca8c1
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2026-03-09 10:00:50 +0100

    merge kernel args
    Signed-off-by: Marcel Lilienthal <134974+mlilien@users.noreply.github.com>

--- recipes-azure-iot/iot-hub-device-update/iot-hub-device-update/swupdate_handler_v2_grub.sh
+++ recipes-azure-iot/iot-hub-device-update/iot-hub-device-update/swupdate_handler_v2_grub.sh
@@ -740,6 +740,8 @@ InstallUpdate() {
                     fi
                     if [ ${ret_val} -eq 0 ]; then
                         swupdate -v -i "${image_file}" -k "${public_key_file}" -e stable,kernelargs &>> "${swupdate_log_file}"
+                        omnect_extra_bootargs.sh
+
                     fi
                 fi
             fi
